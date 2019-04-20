b31eaa1ebe1e21751acfac83163532ae8810f617
German Casares <german.casares@outlook.com>
2019-04-19 19:55:30 -0700

    webpack: Configure icons and scss variables

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+const path = require('path');
 module.exports = {
   transpileDependencies: ['vuex-module-decorators'],
   pwa: {
     name: 'Almanime'
+  },
+  configureWebpack: {
+    resolve: {
+      alias : {
+        "icons": path.resolve(__dirname, "node_modules/vue-material-design-icons")
+      }
+    }    
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/styles/_variables.scss";`
+      }
+    }
   }
 }
