8e3b68227f020c3f9e5a6770782e09417923dcf7
German Casares <german.casares@outlook.com>
2019-04-19 19:57:52 -0700

    component: Add Header component

---
+++ src/app/header/components/Header.vue
@@ -0,0 +1,103 @@
+<template>
+  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link :to="{ name: 'home' }" class="navbar-item">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Almanime: subtitles for all"
+            width="112"
+            height="28"
+          >
+        </router-link>
+
+        <div class="navbar-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link :to="{ name: 'home' }" class="navbar-item">Home</router-link>
+          <a class="navbar-item">Anime</a>
+          <a class="navbar-item">Fansub</a>
+          <a class="navbar-item">Users</a>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field has-addons">
+              <p class="control">
+                <span class="select">
+                  <select>
+                    <option>Anime</option>
+                    <option>Fansub</option>
+                    <option>User</option>
+                  </select>
+                </span>
+              </p>
+              <p class="control">
+                <input class="input" type="text" placeholder="Search for...">
+              </p>
+
+              <p class="control">
+                <a class="button">
+                  <MagnifyIcon title="Search"/>
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div class="navbar-item has-dropdown is-hoverable" v-if="isAuthenticated">
+            <div class="navbar-link">{{ userName }}</div>
+
+            <div class="navbar-dropdown is-right">
+              <a class="navbar-item">Profile</a>
+              <a class="navbar-item">Favourites</a>
+              <a class="navbar-item">Settings</a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">Logout</a>
+            </div>
+          </div>
+
+           <div class="navbar-item" v-else>
+            <div class="field is-grouped">
+              <p class="control">
+                <router-link :to="{ name: 'register' }" class="button is-primary">Register</router-link>
+              </p>
+              <p class="control">
+                <router-link :to="{ name: 'login' }" class="button is-light">Login</router-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MagnifyIcon from 'icons/Magnify.vue';
+
+@Component({
+  components: {
+    MagnifyIcon,
+  },
+})
+export default class Header extends Vue {}
+</script>
+
+<style>
+
+.navbar-item {
+  padding: 0.5rem 1rem;
+}
+
+.navbar-link {
+    min-width: 131px;
+    text-transform: capitalize;
+}
+</style>
