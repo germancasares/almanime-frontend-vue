db8a2fea61522f5f698bb358fecc61295cc18a6d
German Casares <german.casares@outlook.com>
2019-04-19 20:02:33 -0700

    main: Add design dependencies and Axios

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { App } from './app';
 import router from './router';
 import store from './store';
 
+// Design dependencies
+import 'icons/styles.css';
+import Buefy from 'buefy';
+
+// Backend Calls
+import Axios from 'axios';
+Axios.defaults.baseURL = process.env.VUE_APP_APIBASEURL;
+
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy);
 
 new Vue({
   router,
