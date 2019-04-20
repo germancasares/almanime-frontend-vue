76cd93e7f7a3300c24d05adff14c9ba212322ed4
German Casares <german.casares@outlook.com>
2019-04-19 20:02:12 -0700

    router: Remember last position before navigating to another page

--- src/router.ts
+++ src/router.ts
@@ -8,4 +8,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior: (_to, _from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
