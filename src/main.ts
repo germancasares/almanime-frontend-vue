728b1ddda570860b0c0916becf28e60e91e6cb28
German Casares <german.casares@outlook.com>
2020-01-03 19:06:14 -0800

    main: Add router hook for checking if user is authenticated

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,21 @@ Vue.config.productionTip = false;
 
 Vue.use(Buefy);
 
+// Check if route needs to be authorized
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.state.User.account.token) {
+      next({ name: 'login' })
+    } else {
+      next() // go to wherever I'm going
+    }
+  } else {
+    next() // does not require auth, make sure to always call next()!
+  }
+})
+
 new Vue({
   router,
   store,
