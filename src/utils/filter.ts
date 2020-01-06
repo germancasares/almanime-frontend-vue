01ec61fe8eb86abaae23d911828d80fd9e52a10c
German Casares <german.casares@outlook.com>
2020-01-05 17:09:40 -0800

    filter: Add DateTimeFull and FinishedRecount

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,9 +1,12 @@
 import { DateTime } from 'luxon';
+import { EpisodeWithSubtitle } from '@/models';
+import { SubtitleStatus } from '@/enums';
 
-export { DateFull };
+export { DateFull, DateTimeFull, FinishedRecount };
 
-const DateFull = (date: DateTime) => {
-    if (!date) { return ''; }
-    return date.toLocaleString(DateTime.DATE_FULL);
-};
+const DateFull = (date: DateTime): string => date.toLocaleString(DateTime.DATE_FULL);
+
+const DateTimeFull = (date: DateTime): string => date.toLocaleString(DateTime.DATETIME_FULL);
+
+const FinishedRecount = (episodes: EpisodeWithSubtitle[]): number => episodes.filter(e => e.subtitle.status === SubtitleStatus.Published).length
 
