c463a110a9df3b20fde3353df8e9c4a6618a9f52
German Casares <german.casares@outlook.com>
2020-01-06 22:34:32 -0800

    app: Add Fansub store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import { store as home } from './home';
 import { store as anime } from './anime';
 import { store as account } from './account';
-// import { store as fansub } from './fansub';
+import { store as fansub } from './fansub';
 
-export default { home, anime, account };
+export default { home, anime, account, fansub };
