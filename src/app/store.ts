5c965e6f7f2a0682c8eb0c96baee8a8e6bbeb7e6
German Casares <german.casares@outlook.com>
2020-09-17 00:33:36 -0700

    app: Add AppModule as the App component store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,10 +4,40 @@ import { store as account } from './account';
 import { store as fansubView } from './fansub-view';
 import { store as fansubList } from './fansub-list';
 
+import { Module, VuexModule, getModule, MutationAction } from 'vuex-module-decorators';
+import store from '@/store';
+import Helper from '@/utils/helper';
+
+export interface IAppState {
+  isDarkTheme: boolean;
+}
+
+@Module({
+  name: 'App',
+  store,
+  dynamic: true,
+  namespaced: true,
+})
+class AppModule extends VuexModule implements IAppState {
+  public isDarkTheme: boolean = false;
+
+  @MutationAction({ mutate: ['isDarkTheme'] })
+  public async UpdateTheme(isDarkTheme: boolean) {
+    Helper.LocalStorage.Create('isDarkTheme', isDarkTheme);
+
+    return {
+      isDarkTheme,
+    };
+  }
+}
+
+export const app = getModule(AppModule);
+
 export default {
-    home,
-    anime,
-    account,
-    fansubView,
-    fansubList,
+  app,
+  home,
+  anime,
+  account,
+  fansubView,
+  fansubList,
 };
