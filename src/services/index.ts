35f7b41b262216eb12c70fefd0a2d0e1b08baab7
German Casares <german.casares@outlook.com>
2021-03-26 13:24:59 -0700

    services: Add ElasticService and fix webpage casing

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { AccountService } from './accountService';
 export { AnimeService } from './animeService';
 export { FansubService } from './fansubService';
 export { UserService } from './userService';
+export { ElasticService } from './elasticService';
