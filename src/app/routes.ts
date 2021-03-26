08bd34b35c530f0f61e8281e235798b260f9e304
German Casares <german.casares@outlook.com>
2021-03-26 13:30:02 -0700

    routes: Add route for the new subtitle page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,7 @@ import { routes as account } from './account';
 import { routes as fansubView } from './fansub-view';
 import { routes as fansubList } from './fansub-list';
 import { routes as fansubNew } from './fansub-new';
+import { routes as subtitleNew } from './subtitle-new';
 
 export default [
     ...home,
@@ -12,4 +13,5 @@ export default [
     ...fansubNew,
     ...fansubView,
     ...fansubList,
+    ...subtitleNew,
 ];
