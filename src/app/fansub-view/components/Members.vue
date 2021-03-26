08c070dbef5cd690ab569d8be2fd3695d965e75e
German Casares <german.casares@outlook.com>
2021-03-26 13:27:36 -0700

    fansub-view: Add members and Sidebar components

--- src/app/fansub-view/components/Members.vue
+++ src/app/fansub-view/components/Members.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="container">sdadsadsadsadsadsadsadsadsadsadsadsa</div>
+  <div class="container">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent has-image is-2 image is-square" v-for="member in members" :key="member.name">
+        <Tile 
+          :name="member.name"
+          :image="member.avatarUrl"
+          :route="{ name: 'user', params: { name: member.name } }"
+          imageRes="is-square"
+        />
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { mapState } from 'vuex';
 import { FansubUser } from '../../../models';
+import { Tile } from '@/components';
 
 @Component({
+  components: { Tile },
   computed: mapState('FansubView', ['members']),
 })
 export default class Members extends Vue {
